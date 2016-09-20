3f820a529940db879c05f9e066edbe31704d9870
joedarby <joedarby87@gmail.com>
2016-09-20 16:34:42 +0100

    Set up view layout for linepack

--- app/src/main/java/com/darby/joe/gas/NBPLinepackDataActivity.java
+++ app/src/main/java/com/darby/joe/gas/NBPLinepackDataActivity.java
@@ -1,10 +1,12 @@
 package com.darby.joe.gas;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
@@ -45,8 +47,30 @@ public class NBPLinepackDataActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        TextView LPData = (TextView) findViewById(R.id.lpdata);
-                        LPData.setText(data.OLP.toString());
+                        TextView LPDate = (TextView) findViewById(R.id.lp_subheading);
+                        LPDate.setText("Gas day " + data.OLPDate.toString());
+                        TextView sysImbal = (TextView) findViewById(R.id.sysimbal);
+                        sysImbal.setText(String.format(Locale.UK, "%.1f", data.sysImbalance));
+                        TextView underOver = (TextView) findViewById(R.id.under_over);
+                        if (data.sysImbalance >= 0) {
+                            underOver.setText("Forecast oversupply");
+                            sysImbal.setTextColor(Color.rgb(0,200,0));
+                        } else {
+                            underOver.setText("Forecast undersupply");
+                            sysImbal.setTextColor(Color.rgb(200,0,0));
+                        }
+                        TextView olp = (TextView) findViewById(R.id.olp);
+                        olp.setText(String.format(Locale.UK,"%.1f", data.OLP));
+                        TextView pclp = (TextView) findViewById(R.id.pclp);
+                        pclp.setText(String.format(Locale.UK,"%.1f", data.PCLP));
+
+                        TextView dem = (TextView) findViewById(R.id.dem);
+                        dem.setText(String.format(Locale.UK,"%.1f", data.forecastDemand));
+                        TextView flow = (TextView) findViewById(R.id.flow);
+                        flow.setText(String.format(Locale.UK,"%.1f", data.forecastFlow));
+
+                        TextView LPtime = (TextView) findViewById(R.id.LPtime);
+                        LPtime.setText("Forecast by National Grid at " + data.PCLPTime.toString());
                     }
                 });
             }
